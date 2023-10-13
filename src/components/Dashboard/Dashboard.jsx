f788443c77bb109630d2027f44caeeddfcc1d82c
lambeboluwatife <boluwatifelambe@gmail.com>
2023-10-13 20:41:45 +0100

    employer's dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import SideNav from "../SideNav";
 import { dashboardData } from "../../data/services";
 
 const Dashboard = () => {
+  useEffect(() => {
+    document.title = "Hirewave | Employer's Dashboard";
+  }, []);
   return (
     <>
       <div className="column left">
@@ -32,6 +36,25 @@ const Dashboard = () => {
                     <img src={data.img} alt="" />
                   </div>
                 ))}
+                <div className="card card2">
+                  <h4>Suggested Candidates</h4>
+                  <h6>View All</h6>
+                  <h5>None Yet.</h5>
+                </div>
+                <div className="card card2">
+                  <h4>Published Jobs</h4>
+                  <h5>None Yet.</h5>
+                </div>
+                <div className="card card3">
+                  <h4>Applicants</h4>
+                  <div className="card3-content">
+                    <h6>Full Name</h6>
+                    <h6>Job Title</h6>
+                    <h6>Status</h6>
+                    <h6>Date</h6>
+                  </div>
+                  <h5>You have not received any applications yet.</h5>
+                </div>
               </div>
             </div>
           </div>
